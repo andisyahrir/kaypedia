cbd19d16415084a37415657ae4d266706f9b31c2
andisyahrir <71070675+andisyahrir@users.noreply.github.com>
2022-04-02 05:38:20 +0800

    initial

---
+++ android/app/src/main/kotlin/com/sixamtech/efood_multivendor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kaypedia.food
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ web/firebase-messaging-sw.js
@@ -0,0 +1,39 @@
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAojonP3xsLNkLKXqIkml40_6p8EssyyFs",
+  authDomain: "kaypedia-food.firebaseapp.com",
+  projectId: "kaypedia-food",
+  storageBucket: "kaypedia-food.appspot.com",
+  messagingSenderId: "1019654631997",
+  appId: "1:1019654631997:web:436ab38005d69ee2b0b054",
+  databaseURL: "...",
+});
+
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function (payload) {
+    const promiseChain = clients
+        .matchAll({
+            type: "window",
+            includeUncontrolled: true
+        })
+        .then(windowClients => {
+            for (let i = 0; i < windowClients.length; i++) {
+                const windowClient = windowClients[i];
+                windowClient.postMessage(payload);
+            }
+        })
+        .then(() => {
+            const title = payload.notification.title;
+            const options = {
+                body: payload.notification.score
+              };
+            return registration.showNotification(title, options);
+        });
+    return promiseChain;
+});
+self.addEventListener('notificationclick', function (event) {
+    console.log('notification received: ', event)
+});
